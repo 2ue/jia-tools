35c7eb7072c58f7b33b2a04e4e5239441e54f5d9
2ue <jie746635835@163.com>
2024-05-17 12:58:46 +0800

    add string to json function

--- main/index.ts
+++ main/index.ts
@@ -1,3 +1,4 @@
 export * from './get-type';
 export * from './is-type';
 export * from './is-blank-value';
+export * from './is-blank-value';
---
+++ main/string-2-json.ts
@@ -0,0 +1,11 @@
+/**
+ * string to json safe
+ * */
+export function string2Json(str: string, defaultValue?: any) {
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+      console.error(e);
+      return defaultValue;
+    }
+  }
